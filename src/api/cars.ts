74ef52ee1779e5f23179bc6e14300d684e45012a
118815755+voIsung@users.noreply.github.com <jakubmalinski.2002@gmail.com>
2025-06-20 14:29:27 +0200

    Calculate average car rating

--- src/api/cars.ts
+++ src/api/cars.ts
@@ -1,32 +1,48 @@
 import { axiosInstance } from './instance';
-import { Car, RawCar } from '../types';
+import { Car, RawCar, Opinion } from '../types';
+import { getOpinionsByCarId } from './opinions';
 
 export const getCars = async (): Promise<Car[]> => {
-    try {
-        const response = await axiosInstance.get<RawCar[]>(`/cars`);
-        const rawCars = response.data;
-        console.log(response.data)
-        const carImages: Record<string, { default: string }> = import.meta.glob('../assets/cars/*.png', {
-            eager: true
-        });
+  try {
+    const response = await axiosInstance.get<RawCar[]>(`/cars`);
+    const rawCars = response.data;
 
-        return rawCars.map((item) => {
-            const imagePath = `../assets/cars/${item.image_url}`;
-            const imageUrl = carImages[imagePath]?.default || '';
+    const carImages: Record<string, { default: string }> = import.meta.glob('../assets/cars/*.png', {
+      eager: true
+    });
 
-            return {
-                id: item.id,
-                nazwa: item.model.name,
-                kategoria: item.model.bodyType.name,
-                cena: item.cost,
-                image: imageUrl,
-                ocena: 4,   //narazie na sztywno ocena
-                dostepny: item.availability.toLowerCase() === 'available',
-                opis: item.description,
-            };
-        });
-    } catch (error) {
-        console.error('Error fetching cars:', error);
-        return [];
-    }
+    const carsWithRatings: Car[] = await Promise.all(
+      rawCars.map(async (item) => {
+        const imagePath = `../assets/cars/${item.image_url}`;
+        const imageUrl = carImages[imagePath]?.default || '';
+
+        let rating = 0;
+        try {
+          const opinions: Opinion[] = await getOpinionsByCarId(item.id);
+          if (opinions.length > 0) {
+            const sum = opinions.reduce((acc, op) => acc + op.rating, 0);
+            rating = sum / opinions.length;
+          }
+        } catch (e) {
+          console.warn(`Nie udało się pobrać opinii dla auta ${item.id}`);
+        }
+
+        return {
+          id: item.id,
+          nazwa: item.model.name,
+          kategoria: item.model.bodyType.name,
+          cena: item.cost,
+          image: imageUrl,
+          ocena: rating,
+          dostepny: item.availability.toLowerCase() === 'available',
+          opis: item.description,
+        };
+      })
+    );
+
+    return carsWithRatings;
+  } catch (error) {
+    console.error('Error fetching cars:', error);
+    return [];
+  }
 };
